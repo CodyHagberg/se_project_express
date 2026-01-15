f2705f111b61f51680512ab5f8d2764901af540f
CodyHagberg <codyhagberg1117@gmail.com>
2026-01-14 18:39:05 -0700

    Sub 2

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -15,7 +15,7 @@ const createItem = (req, res) => {
     .catch((err) => {
       console.error(err);
       if (err.name === 'ValidationError') {
-        return res.status(BAD_REQUEST).send({ message: err.message });
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
       }
       return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
     });
