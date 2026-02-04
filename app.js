bc25e1667e75513796e1138975d23cda698b5cc5
CodyHagberg <codyhagberg1117@gmail.com>
2026-02-04 14:53:55 -0700

    cors update

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const app = express();
 const { PORT = 3001 } = process.env;
 
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://wearthatweatherradar.csproject.org', 
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use(requestLogger);
